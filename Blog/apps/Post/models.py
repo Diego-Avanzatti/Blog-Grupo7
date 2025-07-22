691d4ed662fa6b8556f39096ad9b8f3200dcfb5e
Diego-Avanzatti <avanzattidiego@gmail.com>
2025-07-22 20:02:22 -0300

    carga de nuevo post

--- Blog/apps/Post/models.py
+++ Blog/apps/Post/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True, default='S/N')
     plataforma = models.ForeignKey(Plataforma, on_delete=models.SET_NULL, null=True, default='S/N')
     activo = models.BooleanField(default=True)
-    imagen_post = models.ImageField(null=True,blank=True, upload_to='Post/images/')
+    imagen_post = models.ImageField(null=True,blank=True, upload_to='posts/')
 
     class Meta:
         ordering = ('-fecha',)
